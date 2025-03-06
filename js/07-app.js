ed1451ee9e7c2fc86d0f7c7fdb703ec427ee271c
Fernando Saucedo <contactofernandosaucedo@gmail.com>
2025-03-06 08:46:35 -0300

    FOR IN Ciclo

---
+++ js/07-app.js
@@ -0,0 +1,16 @@
+const pendientes = ['Tarea', 'Comer', 'Proyecto', 'Estudiar JS'];
+
+const carrito = [
+    { nombre: 'Monitor 27 Pulgadas', precio: 500},
+    { nombre: 'Televisión', precio: 100},
+    { nombre: 'Tablet', precio: 200},
+    { nombre: 'Audifonos', precio: 300},
+    { nombre: 'Teclado', precio: 400},
+    { nombre: 'Celular', precio: 700},
+]
+
+// FOR OF 
+
+for( let pendiente of pendientes ) {
+    console.log(pendiente)
+}
