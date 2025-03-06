05ac536751c0884ca4b348ab412ce1882bdb183f
Fernando Saucedo <contactofernandosaucedo@gmail.com>
2025-03-06 08:28:12 -0300

    ForEach, .map Conceptos Basicos

---
+++ js/06-app.js
@@ -0,0 +1,24 @@
+// forEach
+
+const pendientes = ['Tarea', 'Comer', 'Proyecto', 'Estudiar JS'];
+
+pendientes.forEach( (pendiente, indice) => {
+    console.log(`${indice} : ${pendiente}`)
+})
+
+const carrito = [
+    { nombre: 'Monitor 27 Pulgadas', precio: 500},
+    { nombre: 'Televisión', precio: 100},
+    { nombre: 'Tablet', precio: 200},
+    { nombre: 'Audifonos', precio: 300},
+    { nombre: 'Teclado', precio: 400},
+    { nombre: 'Celular', precio: 700},
+]
+
+const nuevoArreglo = carrito.forEach( producto => producto.nombre);
+
+// .map crea un nuevo arreglo
+const nuevoArreglo2 = carrito.map( producto => producto.nombre);
+
+console.log(nuevoArreglo);
+console.log(nuevoArreglo2);
